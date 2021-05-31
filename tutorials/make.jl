10fc803f62fad934ede5a2b301f80f782674350c
Roger-luo <hiroger@qq.com>
2021-05-31 18:29:03 +0000

    Deploying to gh-pages from  @ bfd454a39149f8a39c6ce92983ef09f25fc0e45f 🚀

--- tutorials/make.jl
+++ tutorials/make.jl
@@ -21,15 +21,6 @@ function generate(title, file)
                 </div>
                 {{ insert foot_general.html }}
             </div>
-            <script>
-                // there is only one pluto notebook load to page each time
-                var iframe = document.getElementsByClassName('plutopage')[0];
-                iframe.onload =  function () {
-                    setTimeout(function() {
-                        iframe.height = iframe.contentWindow.document.body.scrollHeight + 'px';
-                    }, 500);
-                }
-            </script>
         </body>
     </html>
     """
